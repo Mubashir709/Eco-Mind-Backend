91fb0940dc2f81c7ba1b803e8f5ed7e364418535
Mubashir709 <126149757+Mubashir709@users.noreply.github.com>
2025-08-04 06:29:29 +0500

    Add files via upload

--- feedback.js
+++ feedback.js
@@ -1,24 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const Feedback = require('../models/Feedback');
-// const sendNotification = require('../utils/mailer');
-
-// router.post('/', async (req, res) => {
-//   console.log("✅ Feedback route hit");
-
-//   try {
-//     const feedback = new Feedback(req.body);
-//     await feedback.save();
-
-//     await sendNotification(req.body);
-//     res.status(201).json({ message: 'Feedback submitted successfully' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Error saving feedback' });
-//   }
-// });
-
-module.exports = router;
 // routes/feedback.js
 const express = require('express');
 const router = express.Router();
